293cecee3a15787577875402569e17fa6b2bac16
khushij <khushijain264@gmail.com>
2018-08-01 02:07:40 +0530

    models updated

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Members
+from .models import *
 
 
 @admin.register(Members)
 class MembersAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position', 'email',)
+    list_display = ('name', 'enrollment_number', 'position', 'email',)
+    pass
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ('name','desc','created_by','tools',)
+    pass
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name','desc','ev_date',)
+    pass
+@admin.register(Achievements)
+class AchievementsAdmin(admin.ModelAdmin):
+    list_display = ('team','ev_name','college_name',)
+    pass
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('imag','desc',)
+    pass
+@admin.register(UpEvent)
+class UpEventAdmin(admin.ModelAdmin):
+    list_display = ('name','desc','ev_date','registration_url',)
+    pass
+@admin.register(Calendar)
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ('name','ev_date',)
     pass
--- website/models.py
+++ website/models.py
@@ -21,8 +21,11 @@ class Members(models.Model):
     id = models.BigAutoField(primary_key=True)
 
     name = models.CharField(max_length=100, null=True, blank=True)
+    enrollment_number = models.CharField(max_length=11,null =True,blank=True)
+    dept = models.CharField(max_length=6,default='BCA')
     core = models.BooleanField(default=False) # required?
     position = models.CharField(max_length=30, default='Member')
+    
 
     github = models.URLField(null=True, blank=True)
     linkedin = models.URLField(null=True, blank=True)
@@ -40,4 +43,92 @@ class Members(models.Model):
 
     def __str__(self):
         return '{0}'.format(self.id)
+
+class Projects(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=150,null=True)
+    developed = models.BooleanField(default=True)
+    desc = models.TextField(max_length=500,null=True)
+    created_by = models.CharField(max_length=100)
+    tools = models.CharField(max_length=100)
+    photos = models.URLField(null=True, blank=True)
+    url_code = models.URLField(null=True, blank=True)
+    url_project = models.URLField(null=True, blank=True)
+    class Meta:
+        db_table = 'projects'
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return '{0}'.format(self.id)
+
+class Event(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=150)
+    desc = models.TextField(max_length=500)
+    ev_type = models.CharField(max_length =10)
+    ev_date = models.DateField()
+
+    event_head = models.CharField(max_length=100)
+    photos = models.URLField(null=True, blank=True)
+    registration_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return '{0}'.format(self.id)
+    
+class Achievements(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    team = models.CharField(max_length=150)
+    ev_name = models.CharField(max_length=500,blank=True)
+    college_name = models.CharField(max_length =10)
+    fest_name = models.CharField(max_length=100,blank=True)
+    ev_date = models.DateField()
+    
+    photos = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return '{0}'.format(self.id)
+
+class Photo(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    desc = models.TextField(max_length=500)
+    imag = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return '{0}'.format(self.id)
+    
+
+class UpEvent(models.Model):#needs changes
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=150)
+    desc = models.TextField(max_length=500)
+    ev_type = models.CharField(max_length =10)
+    ev_date = models.DateField()
+    event_head = models.CharField(max_length=100)
+    poster = models.URLField(null=True, blank=True)
+    registration_url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return '{0}'.format(self.id)
+
+class Calendar(models.Model):#needs changes
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=150)
+    desc = models.TextField(max_length=500)
+    ev_type = models.CharField(max_length =10)
+    ev_date = models.DateField()
+    event_head = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{0}'.format(self.id)
+
+    
+
+    
+
+
 
