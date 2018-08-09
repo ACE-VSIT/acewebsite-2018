4294bc7f6d197dcab2ca5466f0306353993536e1
Sameer Kumar <sameer18051998@gmail.com>
2018-08-09 19:03:54 +0530

    cancel login

--- ace/settings.py
+++ ace/settings.py
@@ -128,8 +128,8 @@ SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')  # s
 
 SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = ['username', 'first_name', 'last_name', 'email']
 
-SOCIAL_AUTH_BACKEND_ERROR_URL = '/portal/'
-SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = '/portal/'
+# SOCIAL_AUTH_BACKEND_ERROR_URL = '/portal/'
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/portal/'
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
