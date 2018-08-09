4e30bdd49c2f2472943e23220295a530559d2b30
Sameer Kumar <sameer18051998@gmail.com>
2018-08-09 10:40:45 +0530

    magazine

--- ace/urls.py
+++ ace/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from website.views import index
+from website.views import index, magazine
 
 urlpatterns = [
     path('', index),
+    path('magazine/', magazine),
     path('admin/', admin.site.urls),
     path('s3direct/', include('s3direct.urls')),
     path('portal/', include('portalapp.urls')),
--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, template_name='index.html')
+
+
+def magazine(request):
+    return redirect('https://bit.ly/ACEMag2018')
