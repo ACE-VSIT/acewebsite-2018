784c829b83e65b5d83e912401b4c9b0b090d0cd3
Sameer Kumar <sameer18051998@gmail.com>
2018-08-14 19:57:56 +0530

    allowed hosts

--- ace/settings.py
+++ ace/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vipsace.org', 'localhost']
 
 # Application definition
 
