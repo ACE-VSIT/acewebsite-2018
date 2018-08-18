567c0bd2b4e08875652c9cc79f2f403bf976287b
Sameer Kumar <sameer18051998@gmail.com>
2018-08-19 00:50:53 +0530

    admin list display

--- portalapp/admin.py
+++ portalapp/admin.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
 from .models import ACEUserProfile, Tasks, Submissions
 
-admin.site.site_title = 'ACE - RECRUITMENT PORTAL'
-admin.site.site_header = 'ACE - RECRUITMENT PORTAL'
+admin.site.site_title = 'ACE - Selection Portal'
+admin.site.site_header = 'ACE - Selection Portal'
 
 
 class ACEUserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'enroll_number', 'course', 'email_id', 'is_core', 'is_member')
+    list_display = ('name', 'enroll_number', 'course', 'email_id', 'is_core', 'is_member', 'dateUpdated')
     search_fields = ['name__username', 'enroll_number', 'course']
 
 
 class TasksAdmin(admin.ModelAdmin):
-    list_display = ('task_name', 'task_id', 'difficulty_value', 'submission_deadline')
+    list_display = ('task_name', 'task_id', 'difficulty_value', 'total_submissions')
 
 
 class SubmissionsAdmin(admin.ModelAdmin):
-    # list_display = ('task_name', 'task_id', 'difficulty_value', 'submission_deadline')
-	pass
+    list_display = ('user', 'task', 'submission_url', 'dateUpdated')
 
 
 admin.site.register(ACEUserProfile, ACEUserAdmin)
