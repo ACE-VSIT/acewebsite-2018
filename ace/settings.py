64e64c9c8ae281718ccb4cad23435efdb510c04a
Sameer Kumar <sameer18051998@gmail.com>
2018-09-04 21:22:55 +0530

    debug false

--- ace/settings.py
+++ ace/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vipsace.org', 'localhost']
 
@@ -263,8 +263,8 @@ if not DEBUG:
 
 EMAIL_USE_TLS = True
 EMAIL_DEFAULT_SENDER = os.environ.get('EMAIL_DEFAULT_SENDER')
-EMAIL_HOST_USER = os.environ.get('EMAIL')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
 if 'sendgrid' in os.environ.get('EMAIL_CLIENT'):
