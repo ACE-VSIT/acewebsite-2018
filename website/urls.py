50fc88ae2b5b2bf0639cd4b9a73b5473d7e34322
Sameer Kumar <sameer18051998@gmail.com>
2018-09-06 22:38:08 +0530

    structure

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('members/', views.members, name='members'),
+    path('events/', views.events, name='events'),
+    path('projects/', views.projects, name='projects'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('achievements/', views.gallery, name='achievements'),
+    path('calendar/', views.calendar, name='calendar'),
 ]
