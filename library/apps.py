616ed122561bd38700a904b60c1b9a6da8043006
Sameer Kumar <sameer18051998@gmail.com>
2018-09-11 23:30:23 +0530

    cleanup

--- library/apps.py
+++ library/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class FbauthConfig(AppConfig):
-    name = 'acelibraryapp'
+class LibraryConfig(AppConfig):
+    name = 'library'
