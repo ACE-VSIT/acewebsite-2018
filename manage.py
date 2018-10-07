6e2ccb694ae79550a5983bcf123c40fedd1f3071
Ansh Saini <caseymccray201@gmail.com>
2018-10-07 18:38:57 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,9 +4,9 @@ import sys
 
 
 if __name__ == "__main__":
-#   if os.environ.get('DEBUG'):
-    import dotenv
-    dotenv.read_dotenv()
+    if os.environ.get('DEBUG'):
+        import dotenv
+        dotenv.read_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ace.settings")
     try:
         from django.core.management import execute_from_command_line
